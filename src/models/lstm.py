fix type in normalization function from utils

src/models/lstm.py
@@ -5,7 +5,7 @@ import typing as t
 
 from torch.utils.data import DataLoader
 
-from src.models.utils import normalizaion
+from src.models.utils import normalization
 from src.models.utils import point_wise_error, get_anomaly_intervals, detect_point_anomalies, detect_contextual_anomalies
 from src.models.utils import evaluate_point_anomalies, evaluate_collective_anomalies
 from src.models.utils import intervals_to_points, plot_performance
@@ -140,9 +140,9 @@ def run_pipeline(
     train_end = int(T * train_ratio)
     val_end   = int(T * (train_ratio + val_ratio))
 
-    X_train, y_train    = normalizaion(X[:train_end]), y[:train_end][sw:]
-    X_val, y_val        = normalizaion(X[train_end:val_end]), y[train_end:val_end][sw:]
-    X_test, y_test      = normalizaion(X[val_end:]), y[val_end:][sw:]  
+    X_train, y_train    = normalization(X[:train_end]), y[:train_end][sw:]
+    X_val, y_val        = normalization(X[train_end:val_end]), y[train_end:val_end][sw:]
+    X_test, y_test      = normalization(X[val_end:]), y[val_end:][sw:]  
 
     print(f"Series shape : {X.shape}")
     print(f"Train        : timesteps 0 -> {train_end}  ({train_end} steps)")

src/models/lstm_ae.py
@@ -5,7 +5,7 @@ import typing as t
 
 from torch.utils.data import DataLoader
 
-from src.models.utils import normalizaion
+from src.models.utils import normalization
 from src.models.utils import point_wise_error, agg_reconstruction_errors
 from src.models.utils import get_anomaly_intervals, detect_point_anomalies, detect_contextual_anomalies
 from src.models.utils import evaluate_point_anomalies, evaluate_collective_anomalies
@@ -159,9 +159,9 @@ def run_pipeline(
     # a cutoff will be applied to both ends of the signal
     cutoff = sw // 2
 
-    X_train, y_train    = normalizaion(X[:train_end]), y[:train_end][cutoff:-cutoff]
-    X_val, y_val        = normalizaion(X[train_end:val_end]), y[train_end:val_end][cutoff:-cutoff]
-    X_test, y_test      = normalizaion(X[val_end:]), y[val_end:][cutoff:-cutoff]      
+    X_train, y_train    = normalization(X[:train_end]), y[:train_end][cutoff:-cutoff]
+    X_val, y_val        = normalization(X[train_end:val_end]), y[train_end:val_end][cutoff:-cutoff]
+    X_test, y_test      = normalization(X[val_end:]), y[val_end:][cutoff:-cutoff]      
 
     print(f"Series shape : {X.shape}")
     print(f"Train        : timesteps 0 -> {train_end}  ({train_end} steps)")
@@ -197,7 +197,7 @@ def run_pipeline(
     match anomaly_type:
         case "point":
             # compute the forecasting errors for the train set
-            _, y_train_errors = test(model, train_ds, device)
+            _, y_train_errors = test(model, train_ds, sw=sw, ss=ss, device=device)
             # compute the test labels based on the forecasting errors obtained in training
             y_test_hat = detect_point_anomalies(y_train_errors, y_test_errors)
 

src/models/mad_gan.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader
 
 from src.models.signals import SignalsReconstructDataset 
 
-from src.models.utils import normalizaion
+from src.models.utils import normalization
 from src.models.utils import gradient_penalty
 from src.models.utils import point_wise_error, area_wise_error, dtw_error, agg_gan_errors
 from src.models.utils import get_anomaly_intervals, detect_point_anomalies, detect_contextual_anomalies

src/models/tad_gan.py
@@ -9,7 +9,7 @@ from torch.utils.data import DataLoader
 
 from src.models.signals import SignalsReconstructDataset 
 
-from src.models.utils import normalizaion
+from src.models.utils import normalization
 from src.models.utils import gradient_penalty
 from src.models.utils import point_wise_error, area_wise_error, dtw_error, agg_gan_errors
 from src.models.utils import get_anomaly_intervals, detect_point_anomalies, detect_contextual_anomalies
@@ -335,8 +335,8 @@ def run_pipeline(
     # a cutoff will be applied to both ends of the signal
     cutoff = sw // 2
 
-    X_train, y_train    = normalizaion(X[:train_end]), y[:train_end][cutoff:-cutoff]
-    X_test, y_test      = normalizaion(X[train_end:]), y[train_end:][cutoff:-cutoff]      
+    X_train, y_train    = normalization(X[:train_end]), y[:train_end][cutoff:-cutoff]
+    X_test, y_test      = normalization(X[train_end:]), y[train_end:][cutoff:-cutoff]      
 
     print(f"Series shape : {X.shape}")
     print(f"Train        : timesteps 0 -> {train_end}  ({train_end} steps)")

src/models/utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde, zscore
 
 
-def normalizaion(X: np.ndarray):
+def normalization(X: np.ndarray):
     # min-max normalization to [-1, 1]
     X_min = X.min(axis=0)
     X_max = X.max(axis=0)